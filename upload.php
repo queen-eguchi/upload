bccf2251261546d4488a5d105054c2ceb5e6eab4
queen-eguchi <73507666+queen-eguchi@users.noreply.github.com>
2021-03-04 15:01:57 +0900

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+// 一時アップロード先ファイルパス
+$file_tmp  = $_FILES["file_1"]["tmp_name"];
+
+// 正式保存先ファイルパス
+$file_save = "/var/www/html/save/" . $_FILES["file_1"]["name"];
+
+// ファイル移動
+$result = @move_uploaded_file($file_tmp, $file_save);
+if ( $result === true ) {
+    echo "UPLOAD OK";
+} else {
+    echo "UPLOAD NG";
+}
+?>
